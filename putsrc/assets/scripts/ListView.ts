9946681beaeedaa56ced2457015c8c4ea7e1250e
卢乐颜 <luleyan@huohua.cn>
2020-02-03 08:35:22 +0800

    listview滚动

--- putsrc/assets/scripts/ListView.ts
+++ putsrc/assets/scripts/ListView.ts
@@ -31,6 +31,11 @@ export default class ListView extends cc.Component {
     disTopRowPos: number = 0;
     disBtmRowPos: number = 0;
 
+    disTopRowH: number = 0;
+    disBtmRowH: number = 0;
+
+    disCellDict: { [key: number]: ListViewCell } = {};
+
     reuseCells: { [key: string]: ListViewCell[] } = {};
 
     onLoad() {
@@ -66,9 +71,12 @@ export default class ListView extends cc.Component {
             this.disBtmRowIdx = Math.min(Math.floor(disBtm / this.fixedHeight), this.rowCount - 1);
             this.disTopRowPos = this.disTopRowIdx * this.fixedHeight;
             this.disBtmRowPos = this.disBtmRowIdx * this.fixedHeight;
+            this.disTopRowH = this.fixedHeight;
+            this.disBtmRowH = this.fixedHeight;
             for (let rowIdx = this.disTopRowIdx; rowIdx <= this.disBtmRowIdx; rowIdx++) {
                 let cell = this.getUnusedCell(rowIdx);
                 this.setCellPos(cell, rowIdx * this.fixedHeight);
+                this.disCellDict[rowIdx] = cell;
             }
         } else {
             this.disTopRowIdx = 0;
@@ -81,15 +89,18 @@ export default class ListView extends cc.Component {
                 if (nextPos > disTop && !topDone) {
                     this.disTopRowIdx = rowIdx;
                     this.disTopRowPos = curPos;
+                    this.disTopRowH = thisH;
                     topDone = true;
                 }
                 if (topDone) {
                     let cell = this.getUnusedCell(rowIdx);
                     this.setCellPos(cell, curPos);
+                    this.disCellDict[rowIdx] = cell;
                 }
                 if (nextPos >= disBtm) {
                     this.disBtmRowIdx = rowIdx;
                     this.disBtmRowPos = curPos;
+                    this.disBtmRowH = thisH;
                     break;
                 }
                 curPos = nextPos;
@@ -99,15 +110,92 @@ export default class ListView extends cc.Component {
 
     resetContent() {}
 
+    onScrolling() {
+        cc.log('^_^!scrolling', this.content.y);
+        let scrollPos = this.content.y;
+        if (scrollPos <= 0) return; // 差超过底边时return
+
+        let { disTop, disBtm } = this.calcDisplayArea();
+        this.updateDisTopRowData(disTop);
+        this.updateDisBtmRowData(disBtm);
+    }
+
+    updateDisTopRowData(disTop: number) {
+        if (disTop < this.disTopRowPos) {
+            if (this.disTopRowIdx > 0) {
+                this.disTopRowIdx--;
+                this.disTopRowH = this.getRowHeightOnScrolling(this.disTopRowIdx);
+                this.disTopRowPos -= this.disTopRowH;
+
+                let cell = this.getUnusedCell(this.disTopRowIdx);
+                this.setCellPos(cell, this.disTopRowPos);
+                this.disCellDict[this.disTopRowIdx] = cell;
+
+                return this.updateDisTopRowData(disTop);
+            }
+        } else if (this.disTopRowPos + this.disTopRowH <= disTop) {
+            if (this.disTopRowIdx < this.rowCount - 1) {
+                let cell = this.disCellDict[this.disTopRowIdx];
+                this.reclaimCell(cell, this.disTopRowIdx);
+                delete this.disCellDict[this.disTopRowIdx];
+
+                this.disTopRowIdx++;
+                this.disTopRowPos += this.disTopRowH;
+                this.disTopRowH = this.getRowHeightOnScrolling(this.disTopRowIdx);
+
+                return this.updateDisTopRowData(disTop);
+            }
+        }
+    }
+
+    updateDisBtmRowData(disBtm: number) {
+        if (disBtm <= this.disBtmRowPos) {
+            if (this.disTopRowIdx > 0) {
+                let cell = this.disCellDict[this.disBtmRowIdx];
+                this.reclaimCell(cell, this.disBtmRowIdx);
+                delete this.disCellDict[this.disBtmRowIdx];
+
+                this.disBtmRowIdx--;
+                this.disBtmRowH = this.getRowHeightOnScrolling(this.disBtmRowIdx);
+                this.disTopRowPos -= this.disTopRowH;
+
+                return this.updateDisBtmRowData(disBtm);
+            }
+        } else if (this.disBtmRowPos + this.disBtmRowH < disBtm) {
+            if (this.disTopRowIdx < this.rowCount - 1) {
+                this.disBtmRowIdx++;
+                this.disBtmRowPos += this.disBtmRowH;
+                this.disBtmRowH = this.getRowHeightOnScrolling(this.disBtmRowIdx);
+
+                let cell = this.getUnusedCell(this.disBtmRowIdx);
+                this.setCellPos(cell, this.disBtmRowPos);
+                this.disCellDict[this.disBtmRowIdx] = cell;
+
+                return this.updateDisBtmRowData(disBtm);
+            }
+        }
+    }
+
+    getRowHeightOnScrolling(rowIdx: number): number {
+        return this.fixedHeight > 0 ? this.fixedHeight : this.delegate.heightForRow(this, rowIdx);
+    }
+
     /**
      * 区域为正数值，与实际position相反
      */
     calcDisplayArea(): { disTop: number; disBtm: number } {
         let y = this.content.y;
         return {
-            disTop: y - 1,
-            disBtm: y + this.node.height + 1
+            disTop: y,
+            disBtm: y + this.node.height
         };
+    }
+
+    /**
+     * 区域为正数值，与实际position相反
+     */
+    setCellPos(cell: ListViewCell, pos: number) {
+        cell.node.y = -pos;
     }
 
     getUnusedCell(rowIdx: number): ListViewCell {
@@ -119,12 +207,13 @@ export default class ListView extends cc.Component {
 
         let reuseList = this.reuseCells[cellId];
 
-        let unusedCell = null;
+        let unusedCell: ListViewCell = null;
         if (reuseList.length == 0) {
             unusedCell = this.delegate.createCellForRow(this, rowIdx);
             unusedCell.node.parent = this.content;
         } else {
             unusedCell = reuseList.pop();
+            unusedCell.node.active = true;
         }
 
         this.delegate.setCellForRow(this, rowIdx, unusedCell);
@@ -132,14 +221,13 @@ export default class ListView extends cc.Component {
         return unusedCell;
     }
 
-    /**
-     * 区域为正数值，与实际position相反
-     */
-    setCellPos(cell: ListViewCell, pos: number) {
-        cell.node.y = -pos;
-    }
+    reclaimCell(cell: ListViewCell, rowIdx: number) {
+        cell.node.active = false;
 
-    onScrolling() {
-        cc.log('^_^!scrolling');
+        let cellId = this.delegate.cellIdForRow(this, rowIdx);
+        if (!this.reuseCells.hasOwnProperty(cellId)) {
+            this.reuseCells[cellId] = [];
+        }
+        this.reuseCells[cellId].push(cell);
     }
 }
