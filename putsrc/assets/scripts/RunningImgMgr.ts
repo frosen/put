2c291c405d7a96dd27f55dcbbdaa12918c9f831d
luleyan <luleyan@vipkid.com.cn>
2020-11-26 16:02:11 +0800

    1

--- putsrc/assets/scripts/RunningImgMgr.ts
+++ putsrc/assets/scripts/RunningImgMgr.ts
@@ -9,4 +9,18 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export class RunningImgMgr extends cc.Component {
     @property(cc.SpriteFrame) detail: cc.SpriteFrame = null;
+
+    // pet -----------------------------------------------------------------
+
+    @property(cc.SpriteFrame) humanPet: cc.SpriteFrame = null;
+    @property(cc.SpriteFrame) magicPet: cc.SpriteFrame = null;
+    @property(cc.SpriteFrame) mechPet: cc.SpriteFrame = null;
+    @property(cc.SpriteFrame) naturePet: cc.SpriteFrame = null;
+    @property(cc.SpriteFrame) unknownPet: cc.SpriteFrame = null;
+
+    // item -----------------------------------------------------------------
+
+    // pos -----------------------------------------------------------------
+
+    // quester -----------------------------------------------------------------
 }
