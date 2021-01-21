1959577e0808cfe4c2d429fa02ea73760c7fd697
luleyan <luleyan@vipkid.com.cn>
2021-01-21 11:08:24 +0800

    blank cell用于check

---
+++ putsrc/assets/pages/page_act_story/cells/cell_psge_blank/scripts/CellPsgeBlank.ts
@@ -0,0 +1,14 @@
+/*
+ * CellPsgeBlank.ts
+ * 空白
+ * luleyan
+ */
+
+const { ccclass, property } = cc._decorator;
+
+import { ListViewCell } from '../../../../../scripts/ListViewCell';
+
+@ccclass
+export class CellPsgeBlank extends ListViewCell {
+    checkBake() {}
+}
