37bb26e29646460c96a5cfa56d4207ab06c24829
luleyan <luleyan@vipkid.com.cn>
2021-01-27 11:35:56 +0800

    selection

--- putsrc/assets/pages/page_act_story/cells/cell_psge_selection/scripts/CellPsgeSelection.ts
+++ putsrc/assets/pages/page_act_story/cells/cell_psge_selection/scripts/CellPsgeSelection.ts
@@ -9,7 +9,8 @@ const { ccclass, property } = cc._decorator;
 import { CellPsgeBase } from '../../../scripts/CellPsgeBase';
 import { PSBState, PsgeSelctionBtn } from './PsgeSelectionBtn';
 
-const BtnH = 100;
+const SideH = 15;
+const BtnH = 150;
 
 @ccclass
 export class CellPsgeSelection extends CellPsgeBase {
@@ -32,7 +33,7 @@ export class CellPsgeSelection extends CellPsgeBase {
             } else {
                 const btnNode = cc.instantiate(this.btnPrefab);
                 btnNode.parent = this.baseNode;
-                btnNode.y = -BtnH - BtnH * index;
+                btnNode.y = -SideH - (BtnH + SideH) * index;
                 btn = btnNode.getComponent(PsgeSelctionBtn);
                 this.btns.push(btn);
             }
@@ -60,7 +61,7 @@ export class CellPsgeSelection extends CellPsgeBase {
     }
 
     static getHeight(btnLen: number) {
-        return BtnH * 2 + BtnH * btnLen;
+        return SideH + (BtnH + SideH) * btnLen;
     }
 
     onClickBtn(index: number) {}
